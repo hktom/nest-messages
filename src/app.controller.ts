bf54fb853c94a0fb5a935e918e1f8c4489a38941
tom hikari <tomprescott1565@gmail.com>
2024-01-22 15:37:10 +0100

    Add class-validator and new route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/you/:id')
+  getYou(@Param('id') id: string): string {
+    return 'you hello' + id;
   }
 }
